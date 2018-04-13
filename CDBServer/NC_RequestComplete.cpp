98a05cb51a83a71b9a51ac05146616177445ffbc
wangshilei <wslperfect@163.com>
2018-04-13 22:02:08 +0800

    完善NC

--- CDBServer/NC_RequestComplete.cpp
+++ CDBServer/NC_RequestComplete.cpp
@@ -59,7 +59,7 @@ void NC_AcceptCompSuccess(DWORD dwTranstion, void* _lobj, void* _c_bobj)
 
 	if (NULL == memchr(c_bobj->data + c_bobj->dwRecvedCount - 1, 0x0d, 1))
 	{
-		//c_bobj->SetIoRequestFunction(RecvZeroCompFailed, RecvZeroCompSuccess);
+		c_bobj->SetIoRequestFunction(NC_RecvZeroCompFailed, NC_RecvZeroCompSuccess);
 		if (!PostZeroRecv(c_sobj, c_bobj))
 		{
 			_tprintf(_T("客户端信息接收失败, errCode = %d\n"), WSAGetLastError());
@@ -78,4 +78,108 @@ error:
 	freeSObj(c_sobj);
 	freeBObj(c_bobj);
 	return;
+}
+
+void NC_RecvZeroCompFailed(void* _sobj, void* _bobj)
+{
+	SOCKET_OBJ* c_sobj = (SOCKET_OBJ*)_sobj;
+	BUFFER_OBJ* c_bobj = (BUFFER_OBJ*)_bobj;
+
+	CSCloseSocket(c_sobj);
+	freeSObj(c_sobj);
+	freeBObj(c_bobj);
+}
+
+void NC_RecvZeroCompSuccess(DWORD dwTransion, void* _sobj, void* _bobj)
+{
+	SOCKET_OBJ* c_sobj = (SOCKET_OBJ*)_sobj;
+	BUFFER_OBJ* c_bobj = (BUFFER_OBJ*)_bobj;
+
+	c_bobj->SetIoRequestFunction(NC_RecvCompFailed, NC_RecvCompSuccess);
+	if (!PostRecv(c_sobj, c_bobj))
+	{
+		CSCloseSocket(c_sobj);
+		freeSObj(c_sobj);
+		freeBObj(c_bobj);
+		return;
+	}
+}
+
+void NC_RecvCompFailed(void* _sobj, void* _bobj)
+{
+	SOCKET_OBJ* c_sobj = (SOCKET_OBJ*)_sobj;
+	BUFFER_OBJ* c_bobj = (BUFFER_OBJ*)_bobj;
+
+	CSCloseSocket(c_sobj);
+	freeSObj(c_sobj);
+	freeBObj(c_bobj);
+}
+
+void NC_RecvCompSuccess(DWORD dwTransion, void* _sobj, void* _bobj)
+{
+	if (dwTransion <= 0)
+		return NC_RecvCompFailed(_sobj, _bobj);
+
+	SOCKET_OBJ* c_sobj = (SOCKET_OBJ*)_sobj;
+	BUFFER_OBJ* c_bobj = (BUFFER_OBJ*)_bobj;
+
+	c_bobj->dwRecvedCount += dwTransion;
+	if (NULL == memchr(c_bobj->data + c_bobj->dwRecvedCount - 1, 0x0d, 1))
+	{
+		c_bobj->SetIoRequestFunction(NC_RecvZeroCompFailed, NC_RecvZeroCompSuccess);
+		if (!PostZeroRecv(c_sobj, c_bobj))
+		{
+			CSCloseSocket(c_sobj);
+			freeSObj(c_sobj);
+			freeBObj(c_bobj);
+			return;
+		}
+	}
+	else
+	{
+		//ProcessCommand(c_bobj);
+	}
+}
+
+void NC_SendCompFailed(void* _sobj, void* _bobj)
+{
+	SOCKET_OBJ* c_sobj = (SOCKET_OBJ*)_sobj;
+	BUFFER_OBJ* c_bobj = (BUFFER_OBJ*)_bobj;
+
+	CSCloseSocket(c_sobj);
+	freeSObj(c_sobj);
+	freeBObj(c_bobj);
+}
+
+void NC_SendCompSuccess(DWORD dwTransion, void* _sobj, void* _bobj)
+{
+	if (dwTransion <= 0)
+		return NC_SendCompFailed(_sobj, _bobj);
+
+	SOCKET_OBJ* c_sobj = (SOCKET_OBJ*)_sobj;
+	BUFFER_OBJ* c_bobj = (BUFFER_OBJ*)_bobj;
+
+	c_bobj->dwSendedCount += dwTransion;
+	if (c_bobj->dwSendedCount < c_bobj->dwRecvedCount)
+	{
+		if (!PostSend(c_sobj, c_bobj))
+		{
+			CSCloseSocket(c_sobj);
+			freeSObj(c_sobj);
+			freeBObj(c_bobj);
+			return;
+		}
+		return;
+	}
+
+	c_bobj->dwRecvedCount = 0;
+	c_bobj->dwSendedCount = 0;
+	c_bobj->SetIoRequestFunction(NC_RecvZeroCompFailed, NC_RecvZeroCompSuccess);
+	if (!PostZeroRecv(c_sobj, c_bobj))
+	{
+		CSCloseSocket(c_sobj);
+		freeSObj(c_sobj);
+		freeBObj(c_bobj);
+		return;
+	}
 }
--- CDBServer/NC_RequestComplete.h
+++ CDBServer/NC_RequestComplete.h
@@ -2,3 +2,12 @@
 
 void NC_AcceptCompFailed(void* _lobj, void* _c_obj);
 void NC_AcceptCompSuccess(DWORD dwTranstion, void* _lobj, void* _c_bobj);
+
+void NC_RecvZeroCompFailed(void* _sobj, void* _bobj);
+void NC_RecvZeroCompSuccess(DWORD dwTransion, void* _sobj, void* _bobj);
+
+void NC_RecvCompFailed(void* _sobj, void* _bobj);
+void NC_RecvCompSuccess(DWORD dwTransion, void* _sobj, void* _bobj);
+
+void NC_SendCompFailed(void* _sobj, void* _bobj);
+void NC_SendCompSuccess(DWORD dwTransion, void* _sobj, void* _bobj);
